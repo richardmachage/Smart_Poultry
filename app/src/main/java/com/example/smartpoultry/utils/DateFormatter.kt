fc2af8e41c3c278128a4989ca284495aabe0ab0b
richardmachage <rchacha261@gmail.com>
2024-02-08 00:03:35 +0300

    utils package

---
+++ app/src/main/java/com/example/smartpoultry/utils/DateFormatter.kt
@@ -0,0 +1,8 @@
+package com.example.smartpoultry.utils
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun myDateFormatter( date : LocalDate) : String{
+    return DateTimeFormatter.ofPattern("dd MMM yyyy").format(date)
+}
