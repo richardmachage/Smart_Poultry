dd8efdb85e466673fee7539c95c4d25e95cb300b
richardmachage <rchacha261@gmail.com>
2024-02-12 15:09:56 +0300

    block Dao

--- app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/blocks/BlocksDao.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/blocks/BlocksDao.kt
@@ -1,8 +1,22 @@
 package com.example.smartpoultry.data.dataSource.room.entities.blocks
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BlocksDao{
+    @Insert
+    suspend fun addNewBlock(block : Blocks)
 
+    @Delete
+    suspend fun deleteBlock(block : Blocks)
+
+    @Query("SELECT * FROM blocks_tbl")
+    fun getAllBlocks() : Flow<List<Blocks>>
+
+    @Query("SELECT * FROM blocks_tbl WHERE blockId = :blockId")
+    fun getBlock(blockId : Int) : Flow<List<Blocks>>
 }
