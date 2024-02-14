b0f16341a9dd79d346d5c0751473f868013b6ac3
richardmachage <rchacha261@gmail.com>
2024-02-15 01:07:56 +0300

    feeds repository impl

---
+++ app/src/main/java/com/example/smartpoultry/domain/repository/FeedsRepository.kt
@@ -0,0 +1,18 @@
+package com.example.smartpoultry.domain.repository
+
+import com.example.smartpoultry.data.dataSource.room.entities.feeds.Feeds
+import kotlinx.coroutines.flow.Flow
+import java.sql.Date
+
+interface FeedsRepository {
+    suspend fun addNewFeedsRecord(feedRecord: Feeds)
+
+    suspend fun deleteFeedsRecord(feedRecord: Feeds)
+
+    fun getAllFeedsRecords() : Flow<List<Feeds>>
+
+    fun getFeedsRecord(date : Date) : Flow<List<Feeds>>
+
+    fun getFeedRecordsBetween(startDate : Date, endDate : Date) : Flow<List<Feeds>>
+
+}
