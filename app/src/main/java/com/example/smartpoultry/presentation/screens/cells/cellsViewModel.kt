c42ca67b131dd9ac8ac09ec0ebfeb961b48aaef0
richardmachage <rchacha261@gmail.com>
2024-02-18 23:20:13 +0300

    block with cells

--- app/src/main/java/com/example/smartpoultry/presentation/screens/cells/cellsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/cells/cellsViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.smartpoultry.presentation.screens.cells
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.data.dataSource.room.relations.BlocksWithCells
 import com.example.smartpoultry.domain.repository.BlocksRepository
 import com.example.smartpoultry.domain.repository.CellsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class cellsViewModel (
+@HiltViewModel
+class cellsViewModel @Inject constructor(
     private val cellsRepository: CellsRepository,
     private val blocksRepository: BlocksRepository,
 ): ViewModel() {
+
+    var blocksWithCells = mutableStateListOf<BlocksWithCells>()
 }
