2de0c6091fb4cfee59375d33fa1aad530fc64ec8
richardmachage <rchacha261@gmail.com>
2024-02-18 23:18:26 +0300

    cells viewmodel repositories

--- app/src/main/java/com/example/smartpoultry/presentation/screens/cells/cellsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/cells/cellsViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.smartpoultry.presentation.screens.cells
 
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.domain.repository.BlocksRepository
+import com.example.smartpoultry.domain.repository.CellsRepository
 
-class cellsViewModel : ViewModel() {
+class cellsViewModel (
+    private val cellsRepository: CellsRepository,
+    private val blocksRepository: BlocksRepository,
+): ViewModel() {
 }
