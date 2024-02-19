c13a39844801ae52911afcf71e7745ca6fe6d0bf
richardmachage <rchacha261@gmail.com>
2024-02-19 12:33:27 +0300

    getCellsForBlock in UI

--- app/src/main/java/com/example/smartpoultry/presentation/screens/cells/CellsScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/cells/CellsScreen.kt
@@ -1,13 +1,19 @@
 package com.example.smartpoultry.presentation.screens.cells
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.smartpoultry.data.dataSource.room.entities.cells.Cells
 import com.example.smartpoultry.data.dataSource.room.relations.BlocksWithCells
@@ -19,14 +25,24 @@ fun CellsScreen(
     blockId : Int
 ){
     val cellsViewModel = hiltViewModel<CellsViewModel>()
-    //val  listOfCells = cellsViewModel.blocksWithCells[0].cells
-    println("Block Index : ")
+    //val listOfBLocks by cellsViewModel.listOfBlocks.collectAsState()
+    val listOfCells by cellsViewModel.getCellsForBLock(blockId).collectAsState()
+
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Text(text = "BLockId : $blockId")
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(6.dp)
+        ){
+            Text(text = "The Block ID is : $blockId")
+          //  Text(text = "Number of blocks is : ${listOfBLocks.size}")
+            Text(text = "Number of cells is : ${listOfCells.size}")
+        }
+
         /*LazyColumn{
             itemsIndexed(listOfCells){
                 index, item ->
--- app/src/main/java/com/example/smartpoultry/presentation/screens/cells/CellsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/cells/CellsViewModel.kt
@@ -1,18 +1,15 @@
 package com.example.smartpoultry.presentation.screens.cells
 
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.smartpoultry.data.dataSource.room.entities.cells.Cells
-import com.example.smartpoultry.data.dataSource.room.relations.BlocksWithCells
 import com.example.smartpoultry.domain.repository.BlocksRepository
 import com.example.smartpoultry.domain.repository.CellsRepository
-import com.example.smartpoultry.presentation.uiModels.BlockEggCollection
-import com.example.smartpoultry.presentation.uiModels.CellEggCollection
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,41 +17,22 @@ class CellsViewModel @Inject constructor(
     private val cellsRepository: CellsRepository,
     private val blocksRepository: BlocksRepository,
 ): ViewModel() {
+   lateinit var blockId : State<Int>
 
-    val getAllBlocks = blocksRepository.getBlocksWithCells().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(),
-        initialValue = emptyList(),
+
+    val listOfBlocks = blocksRepository.getAllBlocks().stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        emptyList(),
     )
-    var blocksWithCells = mutableStateListOf<BlockEggCollection>()
 
-    init{
-        viewModelScope.launch {
-            getAllBlocks.collect{
-                blocksWithCells.clear()
-                blocksWithCells.addAll(transformBlocksIntoBlocksForInput(it))
-            }
-        }
-    }
+    fun getCellsForBLock(blockId: Int): StateFlow<List<Cells>> {
 
-    private fun transformCellsEntityToCellsWithEggCount(listOfCells: List<Cells>): List<CellEggCollection> {
-        return listOfCells.map { cell ->
-            CellEggCollection(
-                cellId = cell.cellId,
-                cellNum = listOfCells.indexOf(cell) + 1,
-                eggCount = 0
-            )
-        }
-    }
-
-    private fun transformBlocksIntoBlocksForInput(listOfBlocks: List<BlocksWithCells>): List<BlockEggCollection> {
-        return listOfBlocks.map { block ->
-            BlockEggCollection(
-                blockId = block.block.blockId,
-                blockNum = listOfBlocks.indexOf(block) + 1,
-                cells = transformCellsEntityToCellsWithEggCount(block.cell)
-            )
-        }
+        return cellsRepository.getCellsForBlock(blockId).stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            emptyList(),
+        )
     }
 
 }
