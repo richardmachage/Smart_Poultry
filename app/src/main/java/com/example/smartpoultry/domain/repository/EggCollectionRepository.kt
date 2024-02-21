f133a9fe90b90234c671f8e102f854338f7a24b2
richardmachage <rchacha261@gmail.com>
2024-02-21 14:59:54 +0300

    getRecentEggCollectionRecords repo

--- app/src/main/java/com/example/smartpoultry/domain/repository/EggCollectionRepository.kt
+++ app/src/main/java/com/example/smartpoultry/domain/repository/EggCollectionRepository.kt
@@ -1,5 +1,6 @@
 package com.example.smartpoultry.domain.repository
 
+import com.example.smartpoultry.data.dataModels.DailyEggCollection
 import com.example.smartpoultry.data.dataSource.room.entities.eggCollection.EggCollection
 import kotlinx.coroutines.flow.Flow
 import java.sql.Date
@@ -18,5 +19,7 @@ interface EggCollectionRepository{
     fun getAllRecordsForCell(cellId: Int) : Flow<List<EggCollection>>
 
     fun getRecordsForCellBetween(cellId: Int, startDate: Date,endDate: Date) : Flow<List<EggCollection>>
+
+    fun getRecentEggCollectionRecords(startDate: Date) : Flow<List<DailyEggCollection>>
 
 }
