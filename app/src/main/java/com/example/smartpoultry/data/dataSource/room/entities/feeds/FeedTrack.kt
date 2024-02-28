1aa6c27eb5e5488903e6c10d59c2ef53ab62644e
richardmachage <rchacha261@gmail.com>
2024-02-28 14:16:28 +0300

    Feeds Track

--- app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/feeds/FeedTrack.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/feeds/FeedTrack.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.sql.Date
 
-@Entity
+@Entity("feeds_track_tbl")
 data class FeedTrack(
     @PrimaryKey(autoGenerate = true)
     val trackRecordId : Int =0,
