d65e20f730d4d9db652198482d4463ae2f099140
richardmachage <rchacha261@gmail.com>
2024-02-28 13:04:43 +0300

    search feed functionality

--- app/src/main/java/com/example/smartpoultry/presentation/screens/feeds/FeedsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/feeds/FeedsViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.smartpoultry.presentation.screens.feeds
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.data.dataSource.room.entities.feeds.Feeds
 import com.example.smartpoultry.domain.repository.FeedsRepository
+import com.example.smartpoultry.utils.localDateToJavaDate
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import java.sql.Date
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -12,5 +18,16 @@ class FeedsViewModel @Inject constructor(
     private val feedsRepository : FeedsRepository
 ): ViewModel() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     var selectedDate = mutableStateOf(LocalDate.now())
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    var searchDate = mutableStateOf(LocalDate.now())
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onSearchFeedRecord(date : LocalDate) : Flow<List<Feeds>>{
+        return feedsRepository.getFeedsRecord(
+            Date( localDateToJavaDate(searchDate.value))
+        )
+    }
 }
