2270f99fb6580cc8ad07a849adc53961ece23598
richardmachage <rchacha261@gmail.com>
2024-03-07 21:04:58 +0300

    flag cells

--- app/src/main/java/com/example/smartpoultry/domain/trendAnalysis/ProductionAnalysisWorker.kt
+++ app/src/main/java/com/example/smartpoultry/domain/trendAnalysis/ProductionAnalysisWorker.kt
@@ -9,6 +9,7 @@ class ProductionAnalysisWorker (
    private  val params: WorkerParameters
     ) : CoroutineWorker(appContext, params) {
     override suspend fun doWork(): Result {
+
         return Result.success()
     }
 
