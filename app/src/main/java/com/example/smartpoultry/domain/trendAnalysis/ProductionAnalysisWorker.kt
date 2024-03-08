101b28ab95fb3aa7021a0d22abe02737f8d4d111
richardmachage <rchacha261@gmail.com>
2024-03-08 11:10:31 +0300

    Production analysis worker

--- app/src/main/java/com/example/smartpoultry/domain/trendAnalysis/ProductionAnalysisWorker.kt
+++ app/src/main/java/com/example/smartpoultry/domain/trendAnalysis/ProductionAnalysisWorker.kt
@@ -4,13 +4,22 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.smartpoultry.domain.repository.EggCollectionRepository
+import dagger.assisted.Assisted
 
 @HiltWorker
 class ProductionAnalysisWorker (
-    private  val  appContext : Context,
-   private  val params: WorkerParameters
+    @Assisted appContext : Context,
+   @Assisted params: WorkerParameters,
+    private val eggCollectionRepository: EggCollectionRepository
     ) : CoroutineWorker(appContext, params) {
+
+    val  THRESHOLD_RATIO  : Double = 0.5
+    val CONSUCUTIVE_DAYS : Int = 5
+
     override suspend fun doWork(): Result {
+
+
 
         return Result.success()
     }
