05bc398338d76dd418eb67ee859b8c2a391abc4b
richardmachage <rchacha261@gmail.com>
2024-03-08 11:34:12 +0300

    hilt worker configuration

--- app/src/main/java/com/example/smartpoultry/application/SmartPoultryApplication.kt
+++ app/src/main/java/com/example/smartpoultry/application/SmartPoultryApplication.kt
@@ -1,9 +1,19 @@
 package com.example.smartpoultry.application
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
 
 @HiltAndroidApp
-class SmartPoultryApplication : Application() {
+class SmartPoultryApplication : Application(), Configuration.Provider {
+    @Inject lateinit var workerFactory : HiltWorkerFactory
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
 
 }
