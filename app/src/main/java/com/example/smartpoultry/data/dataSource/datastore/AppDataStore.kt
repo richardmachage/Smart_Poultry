fed6df309e7fb76ade03e7e7866b38f2e457cef1
richardmachage <rchacha261@gmail.com>
2024-03-09 21:09:09 +0300

    AppDatastore save data

--- app/src/main/java/com/example/smartpoultry/data/dataSource/datastore/AppDataStore.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/datastore/AppDataStore.kt
@@ -2,10 +2,19 @@ package com.example.smartpoultry.data.dataSource.datastore
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AppDataStore @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
-
+    suspend fun saveData (key : String , value : String){
+        val dataStoreKey = stringPreferencesKey(key)
+        dataStore.edit {myPreferences->
+            myPreferences[dataStoreKey] = value
+        }
+    }
 }
