8e4527ee435aa39f8e1f0a2e0dec3dc951ab4cff
richardmachage <rchacha261@gmail.com>
2024-03-11 10:50:14 +0300

    reports pdf

--- app/src/main/java/com/example/smartpoultry/domain/reports/Report.kt
+++ app/src/main/java/com/example/smartpoultry/domain/reports/Report.kt
@@ -1,5 +1,6 @@
 package com.example.smartpoultry.domain.reports
 
+import android.graphics.pdf.PdfDocument
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -7,5 +8,16 @@ import javax.inject.Inject
 class Report @Inject constructor(
     @ApplicationContext context: ApplicationContext
 ) {
-    
+    companion object{
+        const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
+    }
+
+    fun createAndSavePDF(){
+        val pdfDocument = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(300, 600, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+
+
+
+    }
 }
