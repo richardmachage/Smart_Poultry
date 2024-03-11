7d0af0c922e9428396136b8847decf2d3fc3a5c6
richardmachage <rchacha261@gmail.com>
2024-03-11 22:42:03 +0300

    progress bar

--- app/src/main/java/com/example/smartpoultry/presentation/composables/CircularProgressBar.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/composables/CircularProgressBar.kt
@@ -1,21 +1,24 @@
 package com.example.smartpoultry.presentation.composables
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 @Composable
 fun CircularProgressBar(isLoading: Boolean) {
     if (isLoading) {
         Dialog(onDismissRequest = {}) {
-            Box(contentAlignment = Alignment.Center, modifier = Modifier.size(100.dp)) {
-                CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+            ){
+                CircularProgressIndicator(color = MaterialTheme.colorScheme.tertiary)
             }
         }
     }
