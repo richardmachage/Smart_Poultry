8c2be212c9cad67999ffd887d0af10663336ecc8
richardmachage <rchacha261@gmail.com>
2024-03-12 17:51:38 +0300

    splashscreen view model

---
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/splashScreen/SplaashScreen.kt
@@ -0,0 +1,2 @@
+package com.example.smartpoultry.presentation.screens.splashScreen
+
--- app/src/main/java/com/example/smartpoultry/presentation/screens/splashScreen/SplashViewmodel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/splashScreen/SplashViewmodel.kt
@@ -1,5 +1,8 @@
 package com.example.smartpoultry.presentation.screens.splashScreen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,7 +10,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewmodel @Inject constructor(
-    val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth
 ): ViewModel() {
+    var isLoggedIn by mutableStateOf(false)
 
+    init {
+        checkUser()
+    }
+    private fun checkUser(){
+         firebaseAuth.currentUser?.let {
+            isLoggedIn = true
+        }
+    }
 }
