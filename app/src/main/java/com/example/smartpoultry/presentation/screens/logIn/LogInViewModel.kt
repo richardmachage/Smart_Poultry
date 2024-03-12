6dd2e182a2607f13854c3e29b45c0430e1078f44
richardmachage <rchacha261@gmail.com>
2024-03-12 14:10:52 +0300

    log in function viewmodel

--- app/src/main/java/com/example/smartpoultry/presentation/screens/logIn/LogInViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/logIn/LogInViewModel.kt
@@ -2,8 +2,11 @@ package com.example.smartpoultry.presentation.screens.logIn
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.smartpoultry.domain.repository.FirebaseAuthRepository
+import com.example.smartpoultry.utils.isValidEmail
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class LogInViewModel (
@@ -11,6 +14,40 @@ class LogInViewModel (
 ): ViewModel() {
     var email = mutableStateOf("")
     var password = mutableStateOf("")
+    var validateError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
 
+    fun onLogIn(){
+        isLoading.value = true
+        viewModelScope.launch {
+            if (validateInputs()){
+                val result = firebaseAuthRepository.logIn(email.value, password = password.value)
+                result.onSuccess {
+                    validateError.value = "Log In successsful"
+                }
+                result.onFailure {
+                    validateError.value = "Log in failed: ${it.message.toString()}"
+                }
+            }
+        }
+    }
 
+    private fun validateInputs() : Boolean{
+        if (email.value.isBlank()){
+            validateError.value = "Email field is empty"
+            return false
+        }
+
+        if (password.value.isBlank()){
+            validateError.value = "Password field is empty"
+            return false
+        }
+
+        if (!isValidEmail(email.value)){
+            validateError.value = "Invalid email address"
+            return false
+        }
+
+        return true
+    }
 }
