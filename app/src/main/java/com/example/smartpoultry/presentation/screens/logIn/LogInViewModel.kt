ec5f3125474c6e2b6fef65a77208478a385d10b1
richardmachage <rchacha261@gmail.com>
2024-03-12 13:57:28 +0300

    log in viewmodel

--- app/src/main/java/com/example/smartpoultry/presentation/screens/logIn/LogInViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/logIn/LogInViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.smartpoultry.presentation.screens.logIn
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.smartpoultry.domain.repository.FirebaseAuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,5 +9,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 class LogInViewModel (
     private val firebaseAuthRepository: FirebaseAuthRepository
 ): ViewModel() {
+    var email = mutableStateOf("")
+    var password = mutableStateOf("")
+
 
 }
