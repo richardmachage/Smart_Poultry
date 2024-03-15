6ea02b6af405f6ce14abbe695b37d151257a74bc
richardmachage <rchacha261@gmail.com>
2024-03-15 12:42:01 +0300

    clean up

--- app/src/main/java/com/example/smartpoultry/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/home/HomeViewModel.kt
@@ -3,9 +3,6 @@ package com.example.smartpoultry.presentation.screens.home
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.smartpoultry.data.dataModels.DailyEggCollection
@@ -15,13 +12,11 @@ import com.example.smartpoultry.domain.reports.Report
 import com.example.smartpoultry.domain.repository.BlocksRepository
 import com.example.smartpoultry.domain.repository.CellsRepository
 import com.example.smartpoultry.domain.repository.EggCollectionRepository
-import com.example.smartpoultry.presentation.screens.settingsScreen.PAST_DAYS_KEY
 import com.example.smartpoultry.utils.localDateToJavaDate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import java.sql.Date
 import java.time.LocalDate
 import javax.inject.Inject
@@ -36,15 +31,6 @@ class HomeViewModel @Inject constructor(
     val dataStore: AppDataStore
 ) : ViewModel() {
 
-    var numOfPastDays by mutableIntStateOf(0)
-    var dailyEggCollection = mutableListOf<DailyEggCollection>()
-    init {
-        viewModelScope.launch {
-            dataStore.readData(PAST_DAYS_KEY).collect {
-                numOfPastDays = it.toIntOrNull() ?: 0
-            }
-        }
-    }
 
     val userRole = dataStore.readData(USER_ROLE_KEY).stateIn(
         scope = viewModelScope,
@@ -63,20 +49,6 @@ class HomeViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(),
         initialValue = emptyList()
     )
-
-    val eggCollectionRecords = eggCollectionRepository.getRecentEggCollectionRecords(
-        //Date(LocalDate.now().toEpochDay())
-        Date(
-            getDateDaysAgo(
-                5//numOfPastDays
-            ).toEpochDay()
-        )
-    )
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Eagerly,
-            initialValue = emptyList(),
-        )
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun getOverallCollectionsForPastDays(days : Int): Flow<List<DailyEggCollection>> {
