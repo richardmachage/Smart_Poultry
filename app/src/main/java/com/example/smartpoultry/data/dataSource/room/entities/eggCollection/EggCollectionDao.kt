8b73009da57a7bb91ee818ec2aac5d28f6f06abe
richardmachage <rchacha261@gmail.com>
2024-03-15 12:55:13 +0300

    queries order by date

--- app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/eggCollection/EggCollectionDao.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/eggCollection/EggCollectionDao.kt
@@ -24,54 +24,54 @@ interface EggCollectionDao {
     @Delete
     suspend fun deleteCollectionRecord(eggCollection: EggCollection)
 
-    @Query("SELECT * FROM egg_collection_tbl ORDER BY date DESC")
+    @Query("SELECT * FROM egg_collection_tbl ORDER BY date DESC ")
     fun getAllCollectionRecords(): Flow<List<EggCollection>>
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE date = :date")
+    @Query("SELECT * FROM egg_collection_tbl WHERE date = :date ")
     fun getCollectionRecord(date: Date): Flow<List<EggCollection>>
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE date BETWEEN :startDate AND :endDate ")
+    @Query("SELECT * FROM egg_collection_tbl WHERE date BETWEEN :startDate AND :endDate ORDER BY date DESC ")
     fun getCollectionRecordsBetween(startDate : Date, endDate: Date) : Flow<List<EggCollection>>
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE cellId = :cellId")
+    @Query("SELECT * FROM egg_collection_tbl WHERE cellId = :cellId ORDER BY date DESC")
     fun getAllRecordsForCell(cellId : Int) : Flow<List<EggCollection>>
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE cellId = :cellId AND date BETWEEN :startDate AND  :endDate")
+    @Query("SELECT * FROM egg_collection_tbl WHERE cellId = :cellId AND date BETWEEN :startDate AND  :endDate ORDER BY date DESC")
     fun getRecordsForCellBetween(cellId: Int, startDate: Date,endDate: Date) : Flow<List<EggCollection>>
 
-    @Query("SELECT date, SUM(eggCount) as totalEggs FROM egg_collection_tbl WHERE date >=:startDate GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) as totalEggs FROM egg_collection_tbl WHERE date >=:startDate GROUP BY date ORDER BY date DESC")
     fun getRecentEggCollectionRecords(startDate: Date) : Flow<List<DailyEggCollection>>
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE date>=:startDate AND cellId = :cellId")
+    @Query("SELECT * FROM egg_collection_tbl WHERE date>=:startDate AND cellId = :cellId ORDER BY date DESC")
     fun getCellEggCollectionForPastDays(cellId: Int, startDate: Date) : Flow<List<EggCollection>>//on implementation, calculate date from past number of days input
 
-    @Query("SELECT * FROM egg_collection_tbl WHERE strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth AND cellId=:cellId")
+    @Query("SELECT * FROM egg_collection_tbl WHERE strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth AND cellId=:cellId ORDER BY date DESC")
     fun getCellCollectionByMonth(cellId: Int,yearMonth: String): Flow<List<EggCollection>>
 
 
     @Transaction
-    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId GROUP BY date ORDER BY date DESC")
     fun getBlockEggCollections(blockId: Int) : Flow<List<DailyEggCollection>>
 
     @Transaction
-    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND date>=:startDate GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND date>=:startDate GROUP BY date ORDER BY date DESC")
     fun getBlockEggCollectionsForPastDays(blockId: Int, startDate: Date) : Flow<List<DailyEggCollection>>
 
     @Transaction
-    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND date BETWEEN :startDate AND :endDate GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND date BETWEEN :startDate AND :endDate GROUP BY date ORDER BY date DESC")
     fun getBlockEggCollectionsBetweenDates(blockId: Int, startDate: Date,endDate: Date) : Flow<List<DailyEggCollection>>
 
     @Transaction
-    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl INNER JOIN cells_tbl ON egg_collection_tbl.cellId = cells_tbl.cellId WHERE cells_tbl.blockId = :blockId AND strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth GROUP BY date ORDER BY date DESC")
     fun getBlockCollectionByMonth(blockId: Int, yearMonth: String) : Flow<List<DailyEggCollection>>
 
-    @Query("SELECT date,SUM(eggCount) AS totalEggs FROM egg_collection_tbl WHERE strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth GROUP BY date")
+    @Query("SELECT date,SUM(eggCount) AS totalEggs FROM egg_collection_tbl WHERE strftime('%Y-%m', datetime(date / 1000, 'unixepoch')) = :yearMonth GROUP BY date ORDER BY date DESC")
     fun getOverallCollectionByMonth(yearMonth: String) : Flow<List<DailyEggCollection>>
 
-    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl WHERE date BETWEEN :startDate AND :endDate GROUP BY date")
+    @Query("SELECT date, SUM(eggCount) AS totalEggs FROM egg_collection_tbl WHERE date BETWEEN :startDate AND :endDate GROUP BY date ORDER BY date DESC")
     fun getOverallCollectionBetweenDates(startDate: Date,endDate: Date) : Flow<List<DailyEggCollection>>
 
-    @Query("SELECT date, SUM(eggcount) AS totalEggs FROM egg_collection_tbl WHERE date >= :startDate GROUP BY date")
+    @Query("SELECT date, SUM(eggcount) AS totalEggs FROM egg_collection_tbl WHERE date >= :startDate GROUP BY date ORDER BY date DESC")
     fun getOverallCollectionForPAstDays(startDate: Date) : Flow<List<DailyEggCollection>>
 
 }
