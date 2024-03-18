cb2bb5095fa139647adeeaa010c74665a1b7755c
richardmachage <rchacha261@gmail.com>
2024-03-18 11:10:31 +0300

    view records screen: Lazy column

--- app/src/main/java/com/example/smartpoultry/presentation/screens/viewRecordsScreen/ViewRecordsScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/viewRecordsScreen/ViewRecordsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.smartpoultry.presentation.screens.viewRecordsScreen
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -16,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.smartpoultry.presentation.composables.MyBorderedColumn
+import com.example.smartpoultry.presentation.composables.MyVerticalSpacer
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -45,7 +48,16 @@ fun ViewRecordsScreen(
         ){
             LazyColumn(){
                 itemsIndexed(listOfRecords.value){index, item ->  
+                    MyVerticalSpacer(height = 10)
                     
+                    MyBorderedColumn (
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        Text(text = "Date: ${item.date}")
+                        Text(text = "Date: ${item.cellId}")
+                        Text(text = "Eggs collected: ${item.eggCount}")
+                        Text(text = "Chicken on this day: ${item.henCount}")
+                    }
                 }
             }
         }
