66881a9c93f32492d6527a0b74c5e8f41d525b24
richardmachage <rchacha261@gmail.com>
2024-03-18 10:48:35 +0300

    view records screen: viewmodel

--- app/src/main/java/com/example/smartpoultry/presentation/screens/viewRecordsScreen/ViewRecordsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/viewRecordsScreen/ViewRecordsViewModel.kt
@@ -1,11 +1,29 @@
 package com.example.smartpoultry.presentation.screens.viewRecordsScreen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.smartpoultry.data.dataSource.room.entities.eggCollection.EggCollection
+import com.example.smartpoultry.domain.repository.CellsRepository
+import com.example.smartpoultry.domain.repository.EggCollectionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewRecordsViewModel @Inject constructor(
+    private val eggCollectionRepository: EggCollectionRepository,
+    private val cellsRepository: CellsRepository
+) : ViewModel() {
 
-) : ViewModel(){
+    fun getAllRecords (): Flow<List<EggCollection>> {
+        return eggCollectionRepository.getAllRecords()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(),
+                initialValue = emptyList()
+            )
+    }
+
 }
