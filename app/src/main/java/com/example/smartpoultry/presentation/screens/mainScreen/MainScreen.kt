6b45d1fc4bf0291697ad027596dce00fbbc738d7
richardmachage <rchacha261@gmail.com>
2024-03-18 13:08:22 +0300

    show eggs collection based on user role - implemented

--- app/src/main/java/com/example/smartpoultry/presentation/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/mainScreen/MainScreen.kt
@@ -5,8 +5,13 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.example.smartpoultry.data.dataSource.datastore.USER_ROLE_KEY
 import com.example.smartpoultry.presentation.composables.MyBottomNavBar
 import com.example.smartpoultry.presentation.composables.MyTopAppBar
 import com.example.smartpoultry.presentation.navigation.BottomNavGraph
@@ -20,13 +25,16 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun MainScreen(
     navigator: DestinationsNavigator
 ){
+    val mainViewModel = hiltViewModel<MainScreenViewModel>()
     val navController = rememberNavController()
-
+    val userRole by remember{ mainViewModel.myDataStore.readData(USER_ROLE_KEY)}.collectAsState(
+        initial = " "
+    )
     SmartPoultryTheme {
 
         Scaffold (
             topBar = { MyTopAppBar(navController, navigator) },
-            bottomBar = { MyBottomNavBar(navController) }
+            bottomBar = { MyBottomNavBar(navController, userRole) }
         ){ paddingValues ->
             BottomNavGraph(
                 modifier = Modifier
--- app/src/main/java/com/example/smartpoultry/presentation/screens/mainScreen/MainScreenViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/mainScreen/MainScreenViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.smartpoultry.presentation.screens.mainScreen
 
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.data.dataSource.datastore.AppDataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainScreenViewModel : ViewModel() {
+@HiltViewModel
+class MainScreenViewModel @Inject constructor(
+    private val dataStore: AppDataStore
+): ViewModel() {
 
+    val myDataStore = dataStore
 }
