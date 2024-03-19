b3f9c85eb30d5444577d178bf04565b60aa4dabc
richardmachage <rchacha261@gmail.com>
2024-03-19 08:29:54 +0300

    Alerts dao -> addAlert

--- app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/alerts/AlertsDao.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/alerts/AlertsDao.kt
@@ -1,7 +1,12 @@
 package com.example.smartpoultry.data.dataSource.room.entities.alerts
 
 import androidx.room.Dao
+import androidx.room.Upsert
 
 @Dao
 interface AlertsDao {
+    @Upsert
+    suspend fun addAlert(alert : Alerts) : Long
+
+
 }
