0623422616a8f5d22a031728e39c2ed716937f45
richardmachage <rchacha261@gmail.com>
2024-03-19 10:14:57 +0300

    AlertsScreen -> top App bar

--- app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
@@ -2,19 +2,40 @@ package com.example.smartpoultry.presentation.screens.alerts
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun AlertScreen(
     //modifier:Modifier
+    navigator: DestinationsNavigator
 ){
-    Scaffold {paddingValues ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Alerts")},
+                navigationIcon = {
+                    IconButton(onClick = { navigator.navigateUp() }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription ="back" )
+                    }
+                }
+            )
+        }
+    ) {paddingValues ->
         Surface (
             modifier = Modifier
                 .fillMaxSize()
