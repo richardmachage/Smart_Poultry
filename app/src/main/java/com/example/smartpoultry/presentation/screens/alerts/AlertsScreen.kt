938ca0ae729e871c2967bc0ffa085a66543f1f12
richardmachage <rchacha261@gmail.com>
2024-03-19 10:25:21 +0300

    AlertsScreen -> alert Item

--- app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
@@ -1,7 +1,12 @@
 package com.example.smartpoultry.presentation.screens.alerts
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,10 +18,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.smartpoultry.presentation.composables.MyBorderedRow
+import com.example.smartpoultry.presentation.composables.MyVerticalSpacer
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.text.SimpleDateFormat
 
+@SuppressLint("SimpleDateFormat")
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
@@ -24,6 +38,9 @@ fun AlertScreen(
     //modifier:Modifier
     navigator: DestinationsNavigator
 ){
+    val alertsViewModel = hiltViewModel<AlertsViewModel>()
+    val listOfAlerts by remember { alertsViewModel.getFlaggedCells() }.collectAsState(initial = emptyList())
+    
     Scaffold(
         topBar = {
             TopAppBar(
@@ -43,6 +60,22 @@ fun AlertScreen(
             color = MaterialTheme.colorScheme.background,
 
             ){
+            LazyColumn(){
+                itemsIndexed(listOfAlerts){index, alert ->  
+                    MyVerticalSpacer(height = 5)
+                    MyBorderedRow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(6.dp)
+                    ) {
+                        Column {
+                            Text(text = "Date : ${SimpleDateFormat("dd MMM, yyyy").format(alert.date)}")
+                            Text(text = "Cell : ${alert.cellNum}")
+                            Text(text = "Block : ${alert.blockNum}")
+                        }
+                    }
+                }
+            }
         }
     }
 
