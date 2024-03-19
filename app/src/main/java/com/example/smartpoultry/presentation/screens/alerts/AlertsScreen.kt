1c521da3942c58346edcf46dc22411e2165de704
richardmachage <rchacha261@gmail.com>
2024-03-19 10:44:13 +0300

    AlertsScreen ->

--- app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -80,7 +81,7 @@ fun AlertScreen(
                         }
 
                         IconButton(onClick = { alertsViewModel.onDeleteAlert(alert.alertId) }) {
-
+                            Icon(imageVector = Icons.Default.Delete, contentDescription = "delete" )
                         }
 
                     }
