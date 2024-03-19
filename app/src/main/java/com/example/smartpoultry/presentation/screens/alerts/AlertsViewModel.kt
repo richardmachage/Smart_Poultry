3dfaa42e4a8ec5437f99d9a59ff939ed24733cdd
richardmachage <rchacha261@gmail.com>
2024-03-19 10:08:17 +0300

    AlertsViewModel-> getFlagged cells

--- app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/alerts/AlertsViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.smartpoultry.presentation.screens.alerts
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.smartpoultry.data.dataModels.AlertFull
 import com.example.smartpoultry.domain.repository.AlertsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +15,11 @@ class AlertsViewModel @Inject constructor(
     private val alertsRepository: AlertsRepository
 ) : ViewModel() {
 
+    fun getFlaggedCells(): Flow<List<AlertFull>>{
+        return alertsRepository.getFlaggedCells().stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = emptyList()
+        )
+    }
 }
