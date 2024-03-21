903a428c286997e3a70eebc488070b7c29747e3c
richardmachage <rchacha261@gmail.com>
2024-03-21 23:14:17 +0300

    My searchBar composable

--- app/src/main/java/com/example/smartpoultry/presentation/composables/MySearchBar.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/composables/MySearchBar.kt
@@ -1,8 +1,71 @@
 package com.example.smartpoultry.presentation.composables
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.smartpoultry.presentation.theme.SmartPoultryTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MySearchBar(){
+fun MySearchBar(
+   // isActive : Boolean = false
+    listOfRecords : List<String> = listOf("moday","elena", "enola", "machine", "debryune")
+) {
+    var queryValue by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    var items = remember { mutableStateListOf(listOfRecords) }
+    SearchBar(
+        modifier = Modifier.fillMaxWidth(),
+        query = queryValue,
+        onQueryChange = { newQuery ->
+            queryValue = newQuery
+        },
+        onSearch = {
+                   active = false
+        } ,
+        active = active,
+        onActiveChange = {
+            active = it
+        },
+        placeholder = {
+            Text(text = "Search")
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "search")
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    if (queryValue.isNotBlank()) queryValue = ""
+                    else active = false
+                }) {
+                Icon(imageVector = Icons.Default.Clear, contentDescription = "clear")
+            }
+        }
+    ) {
 
+    }
+}
+
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun MyPrev(){
+    SmartPoultryTheme {
+        MySearchBar()
+    }
 }
