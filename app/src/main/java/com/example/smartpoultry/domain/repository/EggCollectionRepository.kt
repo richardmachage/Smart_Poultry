dc08f2cb409806300dcb70f1fb86dff4e8d43271
richardmachage <rchacha261@gmail.com>
2024-03-21 23:45:07 +0300

    getEggRecordsFull in repo

--- app/src/main/java/com/example/smartpoultry/domain/repository/EggCollectionRepository.kt
+++ app/src/main/java/com/example/smartpoultry/domain/repository/EggCollectionRepository.kt
@@ -1,6 +1,7 @@
 package com.example.smartpoultry.domain.repository
 
 import com.example.smartpoultry.data.dataModels.DailyEggCollection
+import com.example.smartpoultry.data.dataModels.EggRecordFull
 import com.example.smartpoultry.data.dataSource.room.entities.eggCollection.EggCollection
 import kotlinx.coroutines.flow.Flow
 import java.sql.Date
@@ -39,5 +40,7 @@ interface EggCollectionRepository{
     fun getOverallCollectionBetweenDates(startDate: Date,endDate: Date) : Flow<List<DailyEggCollection>>
 
     fun getOverallCollectionForPAstDays(startDate: Date) : Flow<List<DailyEggCollection>>
+
+    fun getAllFullEggCollection() : Flow<List<EggRecordFull>>
 
 }
