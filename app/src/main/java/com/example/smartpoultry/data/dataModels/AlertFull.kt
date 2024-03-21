858d250a91e4acdfecf94e9480f3a8ebc10b62e7
richardmachage <rchacha261@gmail.com>
2024-03-21 23:26:17 +0300

    fun doesMatchSearchQuery in alertFull class

--- app/src/main/java/com/example/smartpoultry/data/dataModels/AlertFull.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataModels/AlertFull.kt
@@ -1,6 +1,8 @@
 package com.example.smartpoultry.data.dataModels
 
+import android.annotation.SuppressLint
 import java.sql.Date
+import java.text.SimpleDateFormat
 
 data class AlertFull(
     val alertId : Int,
@@ -9,7 +11,20 @@ data class AlertFull(
     val cellNum : Int,
     val blockNum : Int
 ){
-    fun doesMatchSearchQuery(){
+    @SuppressLint("SimpleDateFormat")
+    fun doesMatchSearchQuery(query : String) : Boolean{
+        val matchingCombinations = listOf(
+            alertId.toString(),
+            "date ${SimpleDateFormat("dd MMMM yyyy").format(date)}",
+            //"Date ${SimpleDateFormat("dd MMMM yyyy").format(date)}",
+            "cell $cellNum",
+           // "Cell $cellNum",
+            "block $blockNum",
+            //"Block $blockNum",
+        )
 
+        return matchingCombinations.any{
+            it.contains(query, ignoreCase = true)
+        }
     }
 }
