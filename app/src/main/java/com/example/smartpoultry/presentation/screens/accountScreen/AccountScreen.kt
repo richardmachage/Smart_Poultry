e1b62b585033d033d238b80eeefd1fa967ab8d64
richardmachage <rchacha261@gmail.com>
2024-03-30 22:54:33 +0300

    Account screen: Sc afold-> topAPP bar, navigate buttton

--- app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountScreen.kt
@@ -3,7 +3,10 @@ package com.example.smartpoultry.presentation.screens.accountScreen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -28,7 +31,7 @@ fun AccountScreen(
                 title = { Text(text = "Account Details")},
                 navigationIcon = {
                     IconButton(onClick = { navigator.navigateUp() }) {
-                        
+                        Icon(imageVector = Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back")
                     }
                 }
             )
