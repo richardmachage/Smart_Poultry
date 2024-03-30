fb84fa485d9ce57165d0a5b7b8178e70bd5fd406
richardmachage <rchacha261@gmail.com>
2024-03-30 23:00:05 +0300

    Account screen: Sc afold

--- app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountViewModel.kt
@@ -3,9 +3,10 @@ package com.example.smartpoultry.presentation.screens.accountScreen
 import androidx.lifecycle.ViewModel
 import com.example.smartpoultry.domain.repository.FirebaseAuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class AccountViewModel(
+class AccountViewModel @Inject constructor(
     private val fireBaseAuthRepo : FirebaseAuthRepository
 ) : ViewModel() {
     fun registerUser(){
