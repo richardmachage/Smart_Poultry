cbb61f45427c5ef4e96c05bbc9ea608392d4e08d
richardmachage <rchacha261@gmail.com>
2024-03-31 20:36:33 +0300

    Account screen viewmodel: MyDataStore

--- app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/accountScreen/AccountViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.smartpoultry.presentation.screens.accountScreen
 
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.data.dataSource.datastore.AppDataStore
 import com.example.smartpoultry.domain.repository.FirebaseAuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountViewModel @Inject constructor(
-    private val fireBaseAuthRepo : FirebaseAuthRepository
+    private val fireBaseAuthRepo : FirebaseAuthRepository,
+    private val dataStore : AppDataStore
 ) : ViewModel() {
+    val myDataStore = dataStore
     fun registerUser(){
 
     }
