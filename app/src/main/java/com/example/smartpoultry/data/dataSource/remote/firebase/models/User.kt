9e46ab98139a8f2cb3b40eca8e3d83d2f2b9772d
richardmachage <rchacha261@gmail.com>
2024-04-10 13:43:15 +0300

    user class : name and phone

--- app/src/main/java/com/example/smartpoultry/data/dataSource/remote/firebase/models/User.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/remote/firebase/models/User.kt
@@ -1,6 +1,8 @@
 package com.example.smartpoultry.data.dataSource.remote.firebase.models
 
 data class User(
+    val name : String = "",
+    val phone : String = "",
     val email : String = "",
     val role : String = ""
 )
