6d10966180dcee11a8677a96220d362a7d07d38d
richardmachage <rchacha261@gmail.com>
2024-04-10 13:48:03 +0300

    register viewmodel

--- app/src/main/java/com/example/smartpoultry/presentation/screens/registerScreen/RegisterViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/registerScreen/RegisterViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.smartpoultry.presentation.screens.registerScreen
 
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.domain.repository.FirebaseAuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel : ViewModel(){
+class RegisterViewModel @Inject constructor(
+    firebaseAuthRepository: FirebaseAuthRepository
+) : ViewModel(){
 }
