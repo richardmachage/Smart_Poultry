ca0ae4ead5c17dc20fd31fff2e1dc79ba87ba305
richardmachage <rchacha261@gmail.com>
2024-04-18 13:36:38 +0300

    On Boarding Screen:

--- app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/OnBoardingScreen.kt
@@ -1,2 +1,10 @@
 package com.example.smartpoultry.presentation.screens.onBoarding
 
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun OnBoardingScreen(){
+
+}
