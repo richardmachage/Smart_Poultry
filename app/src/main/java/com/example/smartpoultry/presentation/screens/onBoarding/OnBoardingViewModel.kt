dfb1a5ca10e0ddbd9dae01eea99e78be3c231750
richardmachage <rchacha261@gmail.com>
2024-04-18 13:29:04 +0300

    On Boarding Screen:

---
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/OnBoardingViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.smartpoultry.presentation.screens.onBoarding
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OnBoardingViewModel : ViewModel() {
+}
