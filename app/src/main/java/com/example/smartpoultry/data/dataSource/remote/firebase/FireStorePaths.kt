2cc710bf0ee5fe2d7d6a4b67aaa3b234c14a6d30
richardmachage <rchacha261@gmail.com>
2024-04-18 13:11:06 +0300

    firestore paths

---
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/remote/firebase/FireStorePaths.kt
@@ -0,0 +1,7 @@
+package com.example.smartpoultry.data.dataSource.remote.firebase
+
+const val USERS_COLLECTION = "Users"
+const val EGGS_COLLECTION = "EggCollections"
+const val BLOCKS_COLLECTION = "Blocks"
+const val CELLS_COLLECTION = "Cells"
+
