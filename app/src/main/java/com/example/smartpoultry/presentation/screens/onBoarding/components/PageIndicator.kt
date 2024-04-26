63b61d40e5773a333247fba41353b4d7ab350028
richardmachage <rchacha261@gmail.com>
2024-04-26 11:13:03 +0300

    onBoardingScreen: Page Indicator

---
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/components/PageIndicator.kt
@@ -0,0 +1,17 @@
+package com.example.smartpoultry.presentation.screens.onBoarding.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PageIndicator(
+    pageSize: Int,
+    selectedPage: Int,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+    }
+}
