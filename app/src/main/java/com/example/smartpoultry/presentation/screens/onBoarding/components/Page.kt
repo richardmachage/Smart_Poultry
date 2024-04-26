b3f26c08a8905138f5f0ce77c50ad2be9962a861
richardmachage <rchacha261@gmail.com>
2024-04-26 10:43:29 +0300

    onBoardingScreen: Page data class

--- app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/components/Page.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/components/Page.kt
@@ -1,2 +1,9 @@
 package com.example.smartpoultry.presentation.screens.onBoarding.components
 
+import androidx.annotation.DrawableRes
+
+data class Page(
+    val tittle : String,
+    val description : String,
+    @DrawableRes val image : Int
+)
