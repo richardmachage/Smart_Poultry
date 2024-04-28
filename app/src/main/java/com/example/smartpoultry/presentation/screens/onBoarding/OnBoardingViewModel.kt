c474e5bf7ca2ac9c08e0809e9b2898c535b158a3
richardmachage <rchacha261@gmail.com>
2024-04-28 13:20:40 +0300

    onBoardingScreen: savePaaEntry to datastore after onboading

--- app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/onBoarding/OnBoardingViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.smartpoultry.presentation.screens.onBoarding
 
 import androidx.lifecycle.ViewModel
+import com.example.smartpoultry.data.dataSource.datastore.AppDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class OnBoardingViewModel : ViewModel() {
-    fun saveAppEntry(){}
+class OnBoardingViewModel @Inject constructor (
+    val dataStore: AppDataStore
+): ViewModel() {
+    fun saveAppEntry(){
+
+    }
 }
