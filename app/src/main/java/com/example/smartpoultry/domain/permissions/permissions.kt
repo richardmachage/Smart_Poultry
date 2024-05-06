82b21bd0bab129e0318dbfb8dabe8bee8174e54b
richardmachage <rchacha261@gmail.com>
2024-05-06 12:22:01 +0300

    permissions

---
+++ app/src/main/java/com/example/smartpoultry/domain/permissions/permissions.kt
@@ -0,0 +1,7 @@
+package com.example.smartpoultry.domain.permissions
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+const val POST_NOTIFICATIONS = android.Manifest.permission.POST_NOTIFICATIONS
