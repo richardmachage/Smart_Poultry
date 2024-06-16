8975a5ec3ac441112a47f6bd7f0b3da20104b227
richardmachage <rchacha261@gmail.com>
2024-06-16 17:06:16 +0300

    add (Data): BlocksDao, addAll fun

--- app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/blocks/BlocksDao.kt
+++ app/src/main/java/com/example/smartpoultry/data/dataSource/room/entities/blocks/BlocksDao.kt
@@ -2,6 +2,8 @@ package com.example.smartpoultry.data.dataSource.room.entities.blocks
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Upsert
@@ -10,6 +12,10 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BlocksDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(block: List<Blocks>)
+
     @Upsert
     suspend fun addNewBlock(block : Blocks) : Long
 
