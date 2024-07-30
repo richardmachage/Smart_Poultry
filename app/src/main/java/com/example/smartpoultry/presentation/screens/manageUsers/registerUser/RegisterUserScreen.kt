b4d35cf9073e294b66a2e5b0641603712a71d32c
richardmachage <rchacha261@gmail.com>
2024-07-30 21:37:26 +0300

    RegisterNew user screen

---
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/manageUsers/registerUser/RegisterUserScreen.kt
@@ -0,0 +1,8 @@
+package com.example.smartpoultry.presentation.screens.manageUsers.registerUser
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterUserScreen() {
+
+}
---
+++ app/src/main/java/com/example/smartpoultry/presentation/screens/manageUsers/registerUser/RegisterUserViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.smartpoultry.presentation.screens.manageUsers.registerUser
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class RegisterUserViewModel : ViewModel() {
+}
