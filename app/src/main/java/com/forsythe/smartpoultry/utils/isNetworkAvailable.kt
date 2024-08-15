54ddb8fd25927cfabd6bee0e06637770c65c6e48
richardmachage <rchacha261@gmail.com>
2024-08-15 17:09:08 +0300

    time ago clean up

--- app/src/main/java/com/forsythe/smartpoultry/utils/isNetworkAvailable.kt
+++ app/src/main/java/com/forsythe/smartpoultry/utils/isNetworkAvailable.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-fun Context.isNetworkAvailable() : Boolean{
-    val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork = connectivityManager.activeNetwork?: return false
-    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork ?: return false
+    val networkCapabilities =
+        connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
 
-    return when{
+    return when {
         networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) -> true
-        //networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-        //networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
         else -> false
 
     }
